ef0b3d383266cc1dfe0b5ec225e47c3b8f99a980
pingu <giorgiosacchi@me.com>
2023-11-02 17:31:55 +0100

    added context as variable

--- GPyOpt/experiment_design/base.py
+++ GPyOpt/experiment_design/base.py
@@ -2,8 +2,9 @@ class ExperimentDesign(object):
     """
     Base class for all experiment designs
     """
-    def __init__(self, space):
+    def __init__(self, space, context=None):
         self.space = space
+        self.context = context
 
     def get_samples(self, init_points_count):
         raise NotImplementedError("Subclasses should implement this method.")
